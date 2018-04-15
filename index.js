d89d3c58a3f703f5f8095ccc9656229ce45363ac
Eric Wang <erwang01@gmail.com>
2018-04-14 23:06:45 -0700

    remove request import

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
-  request = require("request");
+  //request = require("request");
 
 
 // Sets server port and logs message on success
